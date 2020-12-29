e9e22ad85245b54aa1ea60e45dcaedfa114b3c8e
Gilboot <ssenyonjogilbert@gmail.com>
2020-12-29 12:33:53 +0300

    ready to deploy v2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
 dotenv.config()
-mongoose.connect(process.env.MONGODB_URI)
-// mongoose.connect(process.env.MONGODB_ATLAS_URI)
+// mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_ATLAS_URI)
 app.listen(process.env.PORT, 
     () => `App is running on port ${process.env.PORT}`)
